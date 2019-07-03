c5830725fdda0f5d68689fbc76ccaec2f2746b5c
Preston Harms <prestonharms5@gmail.com>
2019-07-03 10:37:31 -0700

    07-03-19: Added Angular dependencies, ocnfigued spring boot configurations

---
+++ src/main/java/com/inertia/DeveloperNotesApp.java
@@ -0,0 +1,11 @@
+package com.inertia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperNotesApp {
+    public static void main(String args[]){
+        SpringApplication.run(DeveloperNotesApp.class, args);
+    }
+}
